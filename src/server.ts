afca2e6a6b45115210e95e4ddaef7a2f8ceb9a3f
Thiago Skibinski <thiagoskibinski@gmail.com>
2020-06-19 22:58:48 -0300

    feat: add server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express'
-import '@controllers/UsersController'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.get('/', (_req, res) => {
+  return res.json({ message: 'Hello World' })
+});
 
 app.listen(3333)
